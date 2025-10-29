4effc9aa8291263bb10c0db50c56a0ce6c5647b0
benie-joy-possi <beniejoypossi@gmail.com>
2025-10-29 20:07:37 +0100

    style(next.config.js): format code for consistency and readability

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-	output: 'standalone',
-	reactStrictMode: true,
+  output: "standalone",
+  reactStrictMode: true,
 
-	// Allow cross-origin requests in development for browser preview
-	allowedDevOrigins: ["127.0.0.1"],
+  // Allow cross-origin requests in development for browser preview
+  allowedDevOrigins: ["127.0.0.1"],
 
-	/**
-	 * If you are using `appDir` then you must comment the below `i18n` config out.
-	 *
-	 * @see https://github.com/vercel/next.js/issues/41980
-	 */
-	i18n: {
-		locales: ["en"],
-		defaultLocale: "en",
-	},
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 export default config;
